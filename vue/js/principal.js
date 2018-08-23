66d4aa9aaebd34f42aacc392140bf8a9f624e876
JorgeLVM7 <jorgito11@gmail.com>
2018-08-23 00:01:39 -0500

    Se inicializa el proyecto de alpha4

--- vue/js/principal.js
+++ vue/js/principal.js
@@ -1,15 +1,42 @@
 var app = new Vue({
 
     el : '#app',
-    // data: {
-    //     mensaje: 'Hola vuejs!!!! '
-    // },
+
 
     data: {
         lista : [
-            {nombre: 'Luis', promedio: '10', estado: true},
-            {nombre: 'Raquel', promedio: '4', estado: false},
-            {nombre: 'Ester', promedio: '6', estado: true}
+            // {nombre: 'Luis', promedio: '10', estado: true},
+            // {nombre: 'Raquel', promedio: '4', estado: false},
+            // {nombre: 'Ester', promedio: '6', estado: true}
         ],
+
+        nombre : '',
+        promedio : ''
+    },
+
+    methods: {
+
+        agregarNota: function () {
+
+            estadoA =false;
+
+            if (this.promedio>=12.5){
+                estadoA = true;
+            }
+
+            if(this.nombre!="" && this.promedio!=""){
+                this.lista.push({nombre:this.nombre, promedio:this.promedio, estado:estadoA})
+
+                this.nombre="";
+                this.promedio="";
+            }
+            else{
+
+                alert('Ingrese el nombre y promedio del estudiante');
+            }
+
+
+            
+        }
     }
 });
