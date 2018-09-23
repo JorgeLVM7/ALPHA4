2554e79ddc538627b8af8d65f2722ac8afffab60
JorgeLVM7 <jorgito11@gmail.com>
2018-09-23 15:35:26 -0500

    Se agrega plantilla core ui

--- alpha4/resources/views/welcome.blade.php
+++ alpha4/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sistema
                 </div>
 
                 <div class="links">
